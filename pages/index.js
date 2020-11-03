a519416b05d249f2f3af7b7afbd34e3ddb697db9
james-park-songtradr <james.park@songtradr.com>
2020-11-03 14:00:53 -0800

    add first block

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import React from "react";
-import Nav from "./../components/header"
+import Header from "./../components/header"
 import { Button, version, DatePicker } from "antd";
 
 export default Home => (
 	<div>
-		<Nav></Nav>
+		<Header />
+		<section className="main">
+			<div className="main-one">
+				<div className="field">
+					<h1 className="heading-primary heading-primary--main">YouTube Rights Management Made Simple</h1>
+					<h2 className="paragraph-primary">Use Songtradr to monetize and manage your music with YouTube Content Id.</h2>
+					<Button className={`str-btn str-btn--orange str-btn--orange_hover`}>
+						Get started
+					</Button>
+				</div>
+			</div>
+			<div className="main-two">
+				<div className="str-panel">
+
+				</div>
+			</div>
+		</section>
 		{/* <h1 className="heading-primary heading-primary--main">
 			Hello World!!
 		</h1>
